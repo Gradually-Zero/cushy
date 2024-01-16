8a743198642298c695642f88433a499156725b8b
mnz8 <mnz8gg@gmail.com>
2024-01-17 01:42:12 +0800

    feat: 初步跑起来

---
+++ packages/cushy/src/client/error-boundary.tsx
@@ -0,0 +1,103 @@
+import React, { type ReactNode } from 'react';
+import { canUseDOM } from './can-use';
+
+export default class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  override componentDidCatch(error: Error): void {
+    // Catch errors in any components below and re-render with error message
+    if (canUseDOM) {
+      this.setState({ error });
+    }
+  }
+
+  override render(): ReactNode {
+    const { children } = this.props;
+    const { error } = this.state;
+
+    if (error) {
+      const fallbackParams: FallbackParams = {
+        error,
+        tryAgain: () => this.setState({ error: null }),
+      };
+      const fallback: FallbackFunction = this.props.fallback ?? DefaultFallback;
+      return fallback(fallbackParams);
+    }
+
+    // See https://github.com/facebook/docusaurus/issues/6337#issuecomment-1012913647
+    return children ?? null;
+  }
+}
+
+interface ErrorBoundaryProps {
+  readonly fallback?: FallbackFunction;
+  readonly children: ReactNode;
+}
+
+type FallbackFunction = (params: FallbackParams) => JSX.Element;
+
+type FallbackParams = {
+  readonly error: Error;
+  readonly tryAgain: () => void;
+};
+
+type State = {
+  error: Error | null;
+};
+
+const DefaultFallback: FallbackFunction = (params) => <ErrorDisplay {...params} />;
+
+function ErrorDisplay({ error, tryAgain }: ErrorDisplayProps): JSX.Element {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        minHeight: '100vh',
+        width: '100%',
+        maxWidth: '80ch',
+        fontSize: '20px',
+        margin: '0 auto',
+        padding: '1rem',
+      }}
+    >
+      <h1 style={{ fontSize: '3rem' }}>This page crashed</h1>
+      <button
+        type="button"
+        onClick={tryAgain}
+        style={{
+          margin: '1rem 0',
+          fontSize: '2rem',
+          cursor: 'pointer',
+          borderRadius: 20,
+          padding: '1rem',
+        }}
+      >
+        Try again
+      </button>
+      <ErrorBoundaryError error={error} />
+    </div>
+  );
+}
+
+interface ErrorDisplayProps extends FallbackParams {}
+
+function ErrorBoundaryError({ error }: { error: Error }): JSX.Element {
+  const causalChain = getErrorCausalChain(error);
+  const fullMessage = causalChain.map((e) => e.message).join('\n\nCause:\n');
+  return <p style={{ whiteSpace: 'pre-wrap' }}>{fullMessage}</p>;
+}
+
+type CausalChain = [Error, ...Error[]];
+
+export function getErrorCausalChain(error: Error): CausalChain {
+  if (error.cause) {
+    return [error, ...getErrorCausalChain(error.cause as Error)];
+  }
+  return [error];
+}
---
+++ packages/cushy/src/webpack/client.ts
@@ -0,0 +1,57 @@
+import path from 'path';
+import merge from 'webpack-merge';
+import WebpackBar from 'webpackbar';
+import { DefinePlugin } from 'webpack';
+import logger from '../logger';
+import { createBaseConfig } from './base';
+import ChunkAssetPlugin from './plugins/ChunkAssetPlugin';
+import type { Configuration } from 'webpack';
+import type { LoadContext } from '../server/config';
+
+export default async function createClientConfig(props: ClientConfigProps, minify: boolean = true, hydrate: boolean = true): Promise<Configuration> {
+  const isBuilding = process.argv[2] === 'build';
+  const config = await createBaseConfig(props, false, minify);
+
+  const clientConfig = merge(config, {
+    entry: path.resolve(__dirname, '../client/client-entry.js'),
+
+    // module from base
+
+    plugins: [
+      new DefinePlugin({
+        'process.env.HYDRATE_CLIENT_ENTRY': JSON.stringify(hydrate),
+      }),
+      // new ChunkAssetPlugin(),
+      // Show compilation progress bar and build time.
+      new WebpackBar({
+        name: 'Client',
+      }),
+    ],
+
+    optimization: {
+      // Keep the runtime chunk separated to enable long term caching
+      // https://twitter.com/wSokra/status/969679223278505985
+      runtimeChunk: true,
+    },
+  });
+
+  // When building, include the plugin to force terminate building if errors
+  // happened in the client bundle.
+  if (isBuilding) {
+    clientConfig.plugins?.push({
+      apply: (compiler) => {
+        compiler.hooks.done.tap('client:done', (stats) => {
+          if (stats.hasErrors()) {
+            const errorsWarnings = stats.toJson('errors-warnings');
+            logger.error(`Client bundle compiled with errors therefore further build is impossible.\n${errorsWarnings}`);
+            process.exit(1);
+          }
+        });
+      },
+    });
+  }
+
+  return clientConfig;
+}
+
+export type ClientConfigProps = LoadContext & {};
