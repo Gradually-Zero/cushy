cd12627a8a4fb04644498d974b86e9b290678e1e
mnz8 <mnz8@foxmail.com>
2024-03-17 00:40:46 +0800

    updated periodically fix: MDXProvider 不生效问题

--- packages/cushy/src/client/app.tsx
+++ packages/cushy/src/client/app.tsx
@@ -1,13 +1,27 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { RouteNodes } from '@generated/routes';
 import ErrorBoundary from './error-boundary';
+import { MDXProvider } from '@mdx-js/react';
+import { JSX, ClassAttributes, HTMLAttributes } from 'react';
 
 const router = createBrowserRouter([{ path: '*', Component: () => RouteNodes }]);
+
+const components = {
+  p: CustomP,
+};
 
 export default function App(): JSX.Element {
   return (
     <ErrorBoundary>
-      <RouterProvider router={router} />
+      <MDXProvider components={components}>
+        <RouterProvider router={router} />
+      </MDXProvider>
     </ErrorBoundary>
   );
+}
+
+function CustomP(props: JSX.IntrinsicAttributes & ClassAttributes<HTMLDivElement> & HTMLAttributes<HTMLDivElement>) {
+  const { children } = props;
+  console.log('CustomP props', props);
+  return <div style={{ color: 'red', backgroundColor: 'green' }}>{children}</div>;
 }
--- packages/cushy/src/server.constants.ts
+++ packages/cushy/src/server.constants.ts
@@ -12,7 +12,7 @@ export const baseUrl = '/';
 export const staticDirectories = ['static'];
 export const protocol = 'http';
 export const host = 'localhost';
-export const port = 3000;
+export const port = 3001;
 
 /**
  * This does not have extensions, so that we can substitute different ones
