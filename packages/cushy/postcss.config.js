85dee511867e0d871ccb18ba35257f39fa70b6e7
mnz8 <mnz8@foxmail.com>
2024-07-09 23:59:33 +0800

    fix: tailwind 配置文件找不到的问题

--- packages/cushy/postcss.config.js
+++ packages/cushy/postcss.config.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 /** @type {import('postcss').Postcss} */
 module.exports = {
   plugins: {
     autoprefixer: {},
-    tailwindcss: {},
+    tailwindcss: { config: path.join(__dirname, 'tailwind.config.js') },
     'tailwindcss/nesting': {},
     'postcss-import': {},
     'postcss-lightningcss': {
